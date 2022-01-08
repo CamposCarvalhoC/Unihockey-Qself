938e7a2b99094bec83654f6d441483c12cf5a582
Rihen <henri.jaton@gmail.com>
2022-01-08 16:12:37 +0100

    Working site

--- Tests/Flask/app.py
+++ Tests/Flask/app.py
@@ -10,13 +10,20 @@ import plotly.express as px
 
 #define global df
 df = pd.DataFrame()
+df2 = pd.DataFrame()
+
 ################# Utils ###########################
 def read_data_csv(filepath):
-	data = pd.read_csv(filepath,sep=";")
-	data.index = data["Phone timestamp"]
-	data =data.drop(columns=["Phone timestamp","sensor timestamp [ns]"])
+	
+	data = pd.read_csv(filepath,sep=",",index_col=0)
+	#data =data.drop(columns=["Phone timestamp"])
 	data.index = pd.to_datetime(data.index)
-	return data
+	data2=data[["Amount","Type"]].copy()
+	data=data.drop(columns=["Amount","Type"])
+	print(data)
+	data2=data2.head(2)
+	print(data2)
+	return data,data2
 
 ################# FLASK ###########################
 #Init variables
@@ -28,18 +35,20 @@ app.config['UPLOAD_FOLDER']="uploads/"
 
 #set up routes
 @app.route('/')
+@app.route('/upload')
 def upload_file():
     return render_template('upload.html')
 
-@app.route('/upload', methods = ['GET', 'POST'])
+@app.route('/content', methods = ['GET', 'POST'])
 def save_file():
 	global df	
+	global df2	
 	if request.method == 'POST':
 		f = request.files['file']
 		filename = secure_filename(f.filename)
 		print(filename)
 		f.save(app.config['UPLOAD_FOLDER'] + filename)
-		df = read_data_csv(app.config['UPLOAD_FOLDER'] + filename)
+		df,df2 = read_data_csv(app.config['UPLOAD_FOLDER'] + filename)
 		#content = file.read()
 	return content()
 
@@ -47,22 +56,27 @@ def save_file():
 # @app.route("/content")
 def content():
 	global df
+	global df2	
 	#df = read_data_csv("../../data/Passe_avant.csv")
-	fig = px.line(df, x=df.index, y=df.columns,title='Données du match') #,hover_data={"date": "|%B %d, %Y"},
+	fig2 = px.line(df, x=df.index, y=df.columns,title='Données du match') #,hover_data={"date": "|%B %d, %Y"},
+	# fig = px.bar(df,x=df.columns,title='Données du match') #,hover_data={"date": "|%B %d, %Y"},
+	fig=px.bar(df2, x='Type', y='Amount',color='Type')
    #fig.update_xaxes( dtick="M1",tickformat="%b\n%Y")
-	graphList=[]
-	matchGraphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+	barGraphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+	matchGraphJSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
 
 
-	dftest = pd.DataFrame({
-	  'Fruit': ['Apples', 'Oranges', 'Bananas', 'Apples', 'Oranges', 
-	  'Bananas'],
-	  'Amount': [4, 1, 2, 2, 4, 5],
-	  'City': ['SF', 'SF', 'SF', 'Montreal', 'Montreal', 'Montreal']
-	})
-	fig2 = px.bar(dftest, x='Fruit', y='Amount', color='City', 
-	  barmode='group')
-	barGraphJSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
+	# dftest = pd.DataFrame({
+	#   'Fruit': ['Apples', 'Oranges', 'Bananas', 'Apples', 'Oranges', 
+	#   'Bananas'],
+	#   'Amount': [4, 1, 2, 2, 4, 5],
+	#   'City': ['SF', 'SF', 'SF', 'Montreal', 'Montreal', 'Montreal']
+	# })
+	# print(df)
+	# print(dftest)
+	# fig2 = px.bar(dftest, x='Fruit', y='Amount', color='City', 
+	#   barmode='group')
+	# barGraphJSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
 
 	return render_template('content.html', matchGraphJSON=matchGraphJSON,barGraphJSON=barGraphJSON)
 	
